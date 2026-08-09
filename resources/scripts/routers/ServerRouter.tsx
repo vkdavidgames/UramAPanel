850c808eb6c031f4b39c1934c53ce6845aa41c55
vkdg0410 <85492736+vkdg0410@users.noreply.github.com>
2026-08-09 14:08:22 +0200

    Refactor imports and update routing structure
    Signed-off-by: vkdg0410 <85492736+vkdg0410@users.noreply.github.com>

--- resources/scripts/routers/ServerRouter.tsx
+++ resources/scripts/routers/ServerRouter.tsx
@@ -1,46 +1,41 @@
 import TransferListener from '@/components/server/TransferListener';
 import React, { useEffect, useState } from 'react';
-import { NavLink, Route, Switch, useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, NavLink, Route, Switch as RouterSwitch } from 'react-router-dom';
 import NavigationBar from '@/components/NavigationBar';
-import TransitionRouter from '@/TransitionRouter';
 import WebsocketHandler from '@/components/server/WebsocketHandler';
 import { ServerContext } from '@/state/server';
 import { CSSTransition } from 'react-transition-group';
-import Can from '@/components/elements/Can';
 import Spinner from '@/components/elements/Spinner';
-import { NotFound, ServerError } from '@/components/elements/ScreenBlock';
+import { ServerError } from '@/components/elements/ScreenBlock';
 import { httpErrorToHuman } from '@/api/http';
 import { useStoreState } from 'easy-peasy';
 import SubNavigation from '@/components/elements/SubNavigation';
 import InstallListener from '@/components/server/InstallListener';
-import ErrorBoundary from '@/components/elements/ErrorBoundary';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
-import { useLocation } from 'react-router';
-import ConflictStateRenderer from '@/components/server/ConflictStateRenderer';
-import PermissionRoute from '@/components/elements/PermissionRoute';
-import routes from '@/routers/routes';
+
+// EGYEDI RECOVERED COMPONENT IMPORTOK
+import ConfigEditor from '@/components/server/ConfigEditor';
+import ModsContainer from '@/components/server/ModsContainer';
+import ServerDesignEditor from '@/components/server/ServerDesignEditor';
+import PlayerManager from '@/components/server/PlayerManager';
+
+import { NavigationLinks, NavigationRouter } from '@blueprint/extends/routers/ServerRouter';
+import BeforeSubNavigation from '@blueprint/components/Navigation/SubNavigation/BeforeSubNavigation';
+import AdditionalServerItems from '@blueprint/components/Navigation/SubNavigation/AdditionalServerItems';
+import AfterSubNavigation from '@blueprint/components/Navigation/SubNavigation/AfterSubNavigation';
 
 export default () => {
     const match = useRouteMatch<{ id: string }>();
-    const location = useLocation();
 
     const rootAdmin = useStoreState((state) => state.user.data!.rootAdmin);
     const [error, setError] = useState('');
 
     const id = ServerContext.useStoreState((state) => state.server.data?.id);
     const uuid = ServerContext.useStoreState((state) => state.server.data?.uuid);
-    const inConflictState = ServerContext.useStoreState((state) => state.server.inConflictState);
     const serverId = ServerContext.useStoreState((state) => state.server.data?.internalId);
     const getServer = ServerContext.useStoreActions((actions) => actions.server.getServer);
     const clearServerState = ServerContext.useStoreActions((actions) => actions.clearServerState);
-
-    const to = (value: string, url = false) => {
-        if (value === '/') {
-            return url ? match.url : match.path;
-        }
-        return `${(url ? match.url : match.path).replace(/\/*$/, '')}/${value.replace(/^\/+/, '')}`;
-    };
 
     useEffect(
         () => () => {
@@ -74,23 +69,26 @@ export default () => {
             ) : (
                 <>
                     <CSSTransition timeout={150} classNames={'fade'} appear in>
-                        <SubNavigation>
+                        <SubNavigation id={'SubNavigation'}>
+                            <BeforeSubNavigation />
                             <div>
-                                {routes.server
-                                    .filter((route) => !!route.name)
-                                    .map((route) =>
-                                        route.permission ? (
-                                            <Can key={route.path} action={route.permission} matchAny>
-                                                <NavLink to={to(route.path, true)} exact={route.exact}>
-                                                    {route.name}
-                                                </NavLink>
-                                            </Can>
-                                        ) : (
-                                            <NavLink key={route.path} to={to(route.path, true)} exact={route.exact}>
-                                                {route.name}
-                                            </NavLink>
-                                        )
-                                    )}
+                                <NavigationLinks />
+                                <AdditionalServerItems />
+                                
+                                {/* HELYREÁLLÍTOTT TISZTA MENÜPONTOK EMOJIK NÉLKÜL */}
+                                <NavLink to={`/server/${match.params.id}/extensions`} exact>
+                                    Extension Manager
+                                </NavLink>
+                                <NavLink to={`/server/${match.params.id}/config`} exact>
+                                    Config Editor
+                                </NavLink>
+                                <NavLink to={`/server/${match.params.id}/design`} exact>
+                                    Server Design
+                                </NavLink>
+                                <NavLink to={`/server/${match.params.id}/players`} exact>
+                                    Player Manager
+                                </NavLink>
+
                                 {rootAdmin && (
                                     // eslint-disable-next-line react/jsx-no-target-blank
                                     <a href={`/admin/servers/view/${serverId}`} target={'_blank'}>
@@ -98,29 +96,31 @@ export default () => {
                                     </a>
                                 )}
                             </div>
+                            <AfterSubNavigation />
                         </SubNavigation>
                     </CSSTransition>
                     <InstallListener />
                     <TransferListener />
                     <WebsocketHandler />
-                    {inConflictState && (!rootAdmin || (rootAdmin && !location.pathname.endsWith(`/server/${id}`))) ? (
-                        <ConflictStateRenderer />
-                    ) : (
-                        <ErrorBoundary>
-                            <TransitionRouter>
-                                <Switch location={location}>
-                                    {routes.server.map(({ path, permission, component: Component }) => (
-                                        <PermissionRoute key={path} permission={permission} path={to(path)} exact>
-                                            <Spinner.Suspense>
-                                                <Component />
-                                            </Spinner.Suspense>
-                                        </PermissionRoute>
-                                    ))}
-                                    <Route path={'*'} component={NotFound} />
-                                </Switch>
-                            </TransitionRouter>
-                        </ErrorBoundary>
-                    )}
+                    
+                    {/* BELSŐ ULTRA-GYORS REACT ÚTVONALAK BEJEGYZÉSE */}
+                    <RouterSwitch>
+                        <Route path={`${match.path}/extensions`} exact>
+                            <ModsContainer />
+                        </Route>
+                        <Route path={`${match.path}/config`} exact>
+                            <ConfigEditor />
+                        </Route>
+                        <Route path={`${match.path}/design`} exact>
+                            <ServerDesignEditor />
+                        </Route>
+                        <Route path={`${match.path}/players`} exact>
+                            <PlayerManager />
+                        </Route>
+                        <Route path={match.path}>
+                            <NavigationRouter />
+                        </Route>
+                    </RouterSwitch>
                 </>
             )}
         </React.Fragment>
