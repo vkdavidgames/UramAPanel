f521b85f82820c9722711cb0e34715885e9dfdab
vkdg0410 <85492736+vkdg0410@users.noreply.github.com>
2026-08-15 16:04:23 +0200

    Refactor ServerRouter to include new routes
    Signed-off-by: vkdg0410 <85492736+vkdg0410@users.noreply.github.com>

--- resources/scripts/routers/ServerRouter.tsx
+++ resources/scripts/routers/ServerRouter.tsx
@@ -14,6 +14,19 @@ import InstallListener from '@/components/server/InstallListener';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
+// GYÁRI ALAPALDALAK IMPORTOKRA VALÓ HIVATKOZÁSAI
+import ServerConsoleContainer from '@/components/server/console/ServerConsoleContainer';
+import FileManagerContainer from '@/components/server/files/FileManagerContainer';
+import FileEditContainer from '@/components/server/files/FileEditContainer';
+import DatabasesContainer from '@/components/server/databases/DatabasesContainer';
+import ScheduleContainer from '@/components/server/schedules/ScheduleContainer';
+import ScheduleEditContainer from '@/components/server/schedules/ScheduleEditContainer';
+import UsersContainer from '@/components/server/users/UsersContainer';
+import BackupContainer from '@/components/server/backups/BackupContainer';
+import NetworkContainer from '@/components/server/network/NetworkContainer';
+import StartupContainer from '@/components/server/startup/StartupContainer';
+import SettingsContainer from '@/components/server/settings/SettingsContainer';
+
 // EGYEDI RECOVERED COMPONENT IMPORTOK
 import ConfigEditor from '@/components/server/ConfigEditor';
 import ModsContainer from '@/components/server/ModsContainer';
@@ -66,26 +79,25 @@ export default () => {
                     <CSSTransition timeout={150} classNames={'fade'} appear in>
                         <SubNavigation id={'SubNavigation'}>
                             <div>
-                                {/* RECOVERED UTALÁSOK CSONT NÉLKÜL A GYÁRI NAVIGÁCIÓBAN */}
-                                <NavLink to={`/server/${match.params.id}`} exact>
-                                    Console
-                                </NavLink>
-                                <NavLink to={`/server/${match.params.id}/extensions`} exact>
-                                    Extension Manager
-                                </NavLink>
-                                <NavLink to={`/server/${match.params.id}/config`} exact>
-                                    Config Editor
-                                </NavLink>
-                                <NavLink to={`/server/${match.params.id}/design`} exact>
-                                    Server Design
-                                </NavLink>
-                                <NavLink to={`/server/${match.params.id}/players`} exact>
-                                    Player Manager
-                                </NavLink>
+                                {/* Hivatalos Gyári Pterodactyl Alapgombok */}
+                                <NavLink to={`/server/${match.params.id}`} exact>Console</NavLink>
+                                <NavLink to={`/server/${match.params.id}/files`}>File Manager</NavLink>
+                                <NavLink to={`/server/${match.params.id}/databases`}>Databases</NavLink>
+                                <NavLink to={`/server/${match.params.id}/schedules`}>Schedules</NavLink>
+                                <NavLink to={`/server/${match.params.id}/users`}>Users</NavLink>
+                                <NavLink to={`/server/${match.params.id}/backups`}>Backups</NavLink>
+                                <NavLink to={`/server/${match.params.id}/network`}>Network</NavLink>
+                                <NavLink to={`/server/${match.params.id}/startup`}>Startup</NavLink>
+                                <NavLink to={`/server/${match.params.id}/settings`}>Settings</NavLink>
+
+                                {/* A Mi Helyreállított Egyedi Gombjaink */}
+                                <NavLink to={`/server/${match.params.id}/extensions`} exact>Extension Manager</NavLink>
+                                <NavLink to={`/server/${match.params.id}/config`} exact>Config Editor</NavLink>
+                                <NavLink to={`/server/${match.params.id}/design`} exact>Server Design</NavLink>
+                                <NavLink to={`/server/${match.params.id}/players`} exact>Player Manager</NavLink>
 
                                 {rootAdmin && (
-                                    // eslint-disable-next-line react/jsx-no-target-blank
-                                    <a href={`/admin/servers/view/${serverId}`} target={'_blank'}>
+                                    <a href={`/admin/servers/view/${serverId}`} target={'_blank'} rel="noreferrer">
                                         <FontAwesomeIcon icon={faExternalLinkAlt} />
                                     </a>
                                 )}
@@ -96,23 +108,28 @@ export default () => {
                     <TransferListener />
                     <WebsocketHandler />
                     
-                    {/* BELSŐ ÚTVONALAK REGISZTRÁCIÓJA */}
+                    {/* BELSŐ REAT ÚTVONALAK REGISZTRÁCIÓJA (GYÁRI + SAJÁT) */}
                     <RouterSwitch>
-                        <Route path={`${match.path}/extensions`} exact>
-                            <ModsContainer />
-                        </Route>
-                        <Route path={`${match.path}/config`} exact>
-                            <ConfigEditor />
-                        </Route>
-                        <Route path={`${match.path}/design`} exact>
-                            <ServerDesignEditor />
-                        </Route>
-                        <Route path={`${match.path}/players`} exact>
-                            <PlayerManager />
-                        </Route>
+                        <Route path={`${match.path}`} exact><ServerConsoleContainer /></Route>
+                        <Route path={`${match.path}/files`} exact><FileManagerContainer /></Route>
+                        <Route path={`${match.path}/files/edit/:file`} exact><FileEditContainer /></Route>
+                        <Route path={`${match.path}/databases`} exact><DatabasesContainer /></Route>
+                        <Route path={`${match.path}/schedules`} exact><ScheduleContainer /></Route>
+                        <Route path={`${match.path}/schedules/:id`} exact><ScheduleEditContainer /></Route>
+                        <Route path={`${match.path}/users`} exact><UsersContainer /></Route>
+                        <Route path={`${match.path}/backups`} exact><BackupContainer /></Route>
+                        <Route path={`${match.path}/network`} exact><NetworkContainer /></Route>
+                        <Route path={`${match.path}/startup`} exact><StartupContainer /></Route>
+                        <Route path={`${match.path}/settings`} exact><SettingsContainer /></Route>
+                        
+                        {/* SAJÁT MODULOK ÚTVONALAI */}
+                        <Route path={`${match.path}/extensions`} exact><ModsContainer /></Route>
+                        <Route path={`${match.path}/config`} exact><ConfigEditor /></Route>
+                        <Route path={`${match.path}/design`} exact><ServerDesignEditor /></Route>
+                        <Route path={`${match.path}/players`} exact><PlayerManager /></Route>
                     </RouterSwitch>
                 </>
             )}
         </React.Fragment>
     );
-};
+};       
