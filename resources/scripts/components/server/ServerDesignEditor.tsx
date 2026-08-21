237d3852764373d82547d88d6176049255be1553
vkdg0410 <85492736+vkdg0410@users.noreply.github.com>
2026-08-21 10:43:18 +0200

    Fix border color in ServerDesignEditor component
    Signed-off-by: vkdg0410 <85492736+vkdg0410@users.noreply.github.com>

--- resources/scripts/components/server/ServerDesignEditor.tsx
+++ resources/scripts/components/server/ServerDesignEditor.tsx
@@ -327,7 +327,7 @@ export default () => {
                                     backgroundSize: '16px 16px',
                                     imageRendering: 'pixelated'
                                 }}
-                                css={tw`w-full rounded p-3 shadow-2xl relative border border-neutral-950`}
+                                css={tw`w-full rounded p-3 shadow-2xl relative border border-neutral-900`}
                             >
                                 <div css={tw`bg-black bg-opacity-80 border border-neutral-900 rounded p-3 font-mono text-sm shadow-inner flex items-start space-x-3 select-none`}>
                                     
